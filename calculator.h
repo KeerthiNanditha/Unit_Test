1febd0e5c4c8e9c91043aa2456130bd80383d3f6
KeerthiNanditha <36481728+KeerthiNanditha@users.noreply.github.com>
2019-03-29 12:30:35 +0530

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+float add(float num1,float num2);
+float sub(float num1,float num2);
+float mul(float num1,float num2);
+float div(float num1,float num2);
+float cal(float num1,char op,float num2);
