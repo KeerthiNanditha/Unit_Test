082ebaf88dfe4f8a955d8cc992723f07657bfc88
KeerthiNanditha <36481728+KeerthiNanditha@users.noreply.github.com>
2019-03-29 12:57:21 +0530

    Update calculate_Function.c

--- calculate_Function.c
+++ calculate_Function.c
@@ -27,13 +27,13 @@ switch(op){
 float add(float num1,float num2){
 return num1+num2;
 }
-float sub(num1,num2){
+float sub(float num1,float num2){
   return num1-num2;
 }
-float mul(num1,num2){
+float mul(float num1,float num2){
   return num1*num2;
 }
-float div(num1,num2){
+float div(float num1,float num2){
 return num1/num2;
 }
 
