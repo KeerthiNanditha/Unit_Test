381222f8089876e0786e2b1f5fb55833ce8d900f
KeerthiNanditha <36481728+KeerthiNanditha@users.noreply.github.com>
2019-03-29 15:36:14 +0530

    Update calculate_Function.c

--- calculate_Function.c
+++ calculate_Function.c
@@ -1,44 +1,69 @@
-#include "calculator.h"
+#include "Calculator.h"
 
-float calculator(float num1,char op,float num2)
+float calculator(float num1,char op, float num2)
 {
-float result=0.0f;
-switch(op){
-  case '+': 
-    result=add(num1,num2);
-    break;
-  case '-':
-    result=sub(num1,num2);
-    break;
-  case '*':
-    result=mul(num1,num2);
-    break;
-  case '!':
-    if(num2==0){
-      printf("Not supported");
-      return 0;
+    float result=0.0f;
+
+
+       switch(op)
+    {
+        case '+':
+            result = add(num1, num2);
+
+            break;
+
+        case '-':
+            result = sub(num1, num2);
+            break;
+
+        case '*':
+            result = mult(num1, num2);
+            break;
+
+        case '!':{
+            if(num2==0)
+                {
+                    printf("Invalid Divisor '0'!");
+                    return 0;
+                }
+            else
+                result = div(num1, num2);
+                }
+            break;
+
+        default:
+            printf("Invalid operator");
     }
-    else{
-      
-    result=div(num1,num2);
-    }
-    break;
-    deafult:
-    printf("Enter invalid operator");
+
+
+    //printf("%.2f %c %.2f = %.2f", num1, op, num2, result);
+
+    return result;
 }
-  return result;
+
+
+
+float add(float num1, float num2)
+{
+
+    return num1 + num2;
 }
-float add(float num1,float num2){
-return num1+num2;
+
+float sub(float num1, float num2)
+{
+    return num1 - num2;
 }
-float sub(float num1,float num2){
-  return num1-num2;
+
+
+float mult(float num1, float num2)
+{
+    return num1 * num2;
 }
-float mul(float num1,float num2){
-  return num1*num2;
-}
-float div(float num1,float num2){
-return num1/num2;
+
+
+float div(float num1, float num2)
+{
+    return num1 / num2;
 }
 
   
